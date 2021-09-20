69200f99078135e7a6e48a7679e334e097908941
Tal Hayut <talha@accessibe.com>
2021-09-20 17:16:29 +0300

    chore: no test

--- test.js
+++
@@ -1,9 +0,0 @@
-const style = require('styl3');
-// use decorators for bold, underline etc...
-const s = style({theme: 'pastel', decorators: { bold: '_' }});
-s.red`this is the new _BOLD_ @RED@`
-// double decoratros
-s.cyan`this is !*important*!`
-s.green`viva la %italia%`
-s.pink`address: ~you@somewhere.earth~`
-  
